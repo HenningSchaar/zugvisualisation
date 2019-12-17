80a5a25242e7592df3bebcfe193be11157f2c9ec
Henning Schaar <henning.schaar@gmail.com>
2019-12-17 11:45:53 +0100

    Add Pixeldata to map and draw a map

--- sketch.js
+++ sketch.js
@@ -1,45 +1,76 @@
 let trains;
+let stationList;
 let trainsOld;
+let map;
+let pixelMap = {};
 const backgroundColor = 50;
 const trainColour1 = [255, 0, 0];
 const trainColour2 = [255, 0, 255];
 const trainColour3 = [0, 255, 0];
 const trainColour4 = [0, 255, 0];
-const sizeY = 1440;
-const sizeX = sizeY * (16 / 9)
+const sizeY = 1200;
+const sizeX = sizeY * (16 / 10)
+const latOffset = 47;
+const lngOffset = 6.5;
+const coordRange = 3.4;
+
+function preload() {
+    map = loadJSON('map.json');
+}
+
 
 function setup() {
+    stationList = map.stations;
+    addPixelData(stationList);
+    //console.log(stationList);
     createCanvas(sizeX, sizeY);
     background(backgroundColor);
     frameRate(1);
-    textSize(32);
+    textSize(10);
     noStroke();
+    drawMap(stationList);
+    noLoop();
 }
 
 function draw() {
-    getData();
-    if (trains) {
-        drawTrains(trains);
+
+}
+
+function addPixelData(stationList) {
+    stationList.forEach(stationEntry => {
+        station = new Station(stationEntry);
+        stationEntry.coordinates["x"] = station.x;
+        stationEntry.coordinates["y"] = station.y;
+    })
+
+}
+
+class Station {
+    constructor(stationEntry) {
+        this.name = stationEntry.name;
+        this.lat = stationEntry.coordinates.lat;
+        this.lng = stationEntry.coordinates.lng;
+        this.x = (stationEntry.coordinates.lng - lngOffset) * (height / coordRange) + (width / 2 - height / 2);
+        this.y = height - ((stationEntry.coordinates.lat - latOffset) * (height / coordRange));
+        this.diameter = 10;
+        this.color = [random(50, 255), random(50, 255), random(50, 255)];
+        this.speed = 0;
+    }
+
+    display(arg) {
+        fill(this.color)
+        ellipse(this.x, this.y, this.diameter, this.diameter);
+        if (arg == text) {
+            text(this.name, this.x + 5, this.y + 5)
+        }
     }
 }
 
-function getData() {
-    let url =
-        'https://www.zugfinder.de/js/json.php?netz=deutschland|439|53|1278|615';
-    httpGet(url, 'json', false, function(response) {
-        if (trains != response) {
-            trains = response.array; //convert train data to an array of trains
-            trains.shift(); //remove first entry which for some reason is always empty
-        } else { console.log('No new data.') };
-    })
-}
-
-function drawTrains(trains) {
-    background(255);
+function drawMap(stationList) {
     background(backgroundColor);
-    trains.forEach(trainDataEntry => {
-        train = new Train(trainDataEntry);
-        train.display();
+    stationList.forEach(stationEntry => {
+        station = new Station(stationEntry);
+        station.display();
     });
 }
 
@@ -57,6 +88,22 @@ class Train {
         ellipse(this.x, this.y, this.diameter, this.diameter);
     }
 }
+
+function drawTrains(trains) {
+    background(255);
+    background(backgroundColor);
+    trains.forEach(trainDataEntry => {
+        train = new Train(trainDataEntry);
+        train.display();
+    });
+}
+
+function handleError(error) {
+    console.log(error);
+    setTimeout(perform, 1000);
+};
+
+/*
 
 function extractColour(colour) {
     if (colour == "rot") {
@@ -79,8 +126,14 @@ function extractColour(colour) {
     }
 }
 
-
-function handleError(error) {
-    console.log(error);
-    setTimeout(perform, 1000);
-};
+function getData() {
+    let url =
+        'https://www.zugfinder.de/js/json.php?netz=deutschland|439|53|1278|615';
+    httpGet(url, 'json', false, function(response) {
+        if (trains != response) {
+            trains = response.array; //convert train data to an array of trains
+            trains.shift(); //remove first entry which for some reason is always empty
+        } else { console.log('No new data.') };
+    })
+}
+*/
